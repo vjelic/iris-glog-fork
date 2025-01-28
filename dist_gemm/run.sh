b3b7ed1f875bf25b7675c26ef1277e2ef407dd22
Muhammad Awad <MuhammadAbdelghaffar.Awad@amd.com>
2025-01-28 13:10:52 -0600

    Add all reduce
    Signed-off-by: Muhammad Awad <MuhammadAbdelghaffar.Awad@amd.com>

--- dist_gemm/run.sh
+++ dist_gemm/run.sh
@@ -1,5 +1,6 @@
-num_gpus=4
+num_gpus=2
 export OMP_NUM_THREADS=1
 # collective="all_gather"
 collective="all_reduce"
-python -m torch.distributed.run --nproc_per_node=${num_gpus} ${collective}_dist_gemm_B.py
+# python -m torch.distributed.run --nproc_per_node=${num_gpus} ${collective}_dist_gemm_B.py
+python -m torch.distributed.run --nproc_per_node=${num_gpus} ${collective}.py
