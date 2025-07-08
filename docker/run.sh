fc763701037ec4e87f3c14a3502e775fb21ccd11
root <root@ctr-cx63-mi300x-21.amd.com>
2025-07-08 22:57:58 +0000

    First push

--- docker/run.sh
+++ docker/run.sh
@@ -5,5 +5,5 @@
 
 IMAGE_NAME=${1:-"iris-dev"}
 
-alias drun='docker run -it --network=host --device=/dev/kfd --device=/dev/dri --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME:$HOME -w $HOME --shm-size=16G --ulimit memlock=-1 --ulimit stack=67108864'
+alias drun='docker run -it --network=host --device=/dev/kfd --device=/dev/dri --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME:$HOME -v $HOME/.ssh:/root/.ssh:ro -w $HOME --shm-size=16G --ulimit memlock=-1 --ulimit stack=67108864'
 drun $IMAGE_NAME
